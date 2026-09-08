72cf2ba20b9203cf8a607d186b8a3910e1eee4c3
fellype kenned <120345767+Fellypr@users.noreply.github.com>
2026-09-08 09:39:09 -0300

    feat: adiciona arquivos de configuração e modelos para a API

---
+++ Models/Projects.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace portfolioApi.Models
+{
+    public class Projects
+    {
+        [Key]
+        public int Id { get; set; }
+        public string TitleProject { get; set; }
+        public string Description { get; set; }
+        public string UrlImage { get; set; }
+        public string Status { get; set; }
+    
+    }
+}
